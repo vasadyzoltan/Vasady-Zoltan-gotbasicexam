b7d0c88c4e8f26166b98cb2937243fe5b621d4d5
Zoltán Vasady <vzol@freestart.hu>
2018-07-23 12:14:58 +0200

    first steps

---
+++ js/got.js
@@ -0,0 +1,31 @@
+function getData(url, callbackFunc) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            callbackFunc(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function successAjax(xhttp) {
+    // itt a json content, benne a data változóban
+    var userDatas = JSON.parse(xhttp.responseText);
+    console.log(userDatas);
+    /*
+      Pár sorral lejebb majd ezt olvashatod:
+      IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ!
+
+      Na azokat a függvényeket ITT HÍVD MEG! 
+
+      A userDatas NEM GLOBÁLIS változó, ne is tegyétek ki globálisra. Azaz TILOS!
+      Ha valemelyik függvényeteknek kell, akkor paraméterként adjátok át.
+    */
+}
+
+// Írd be a json fileod nevét/útvonalát úgy, ahogy nálad van
+getData('/json/aJsonFileodNeve.json', successAjax);
+
+// Live servert használd mindig!!!!!
+/* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
